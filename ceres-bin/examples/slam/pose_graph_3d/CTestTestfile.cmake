7f0ba2c1170fafaa79548cf5893e07639720d188
nsk06 <nonidh.singh@gmail.com>
2021-04-24 02:25:51 +0530

    files

---
+++ ceres-bin/examples/slam/pose_graph_3d/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nsk06/Documents/sem 8/CV/project/project-brown-munde/ceres-solver/examples/slam/pose_graph_3d
+# Build directory: /home/nsk06/Documents/sem 8/CV/project/project-brown-munde/ceres-bin/examples/slam/pose_graph_3d
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
